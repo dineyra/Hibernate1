64fefcbb12585473eb55254940d056ffda79257c
dineyra <dineyra06@gmail.com>
2022-11-30 23:43:02 +0300

    P.P 1.1.3

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,29 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
+    private final static UserService userService = new UserServiceImpl();
+    public static void main(String[] args) throws SQLException {
         // реализуйте алгоритм здесь
+
+        userService.createUsersTable();
+
+        userService.saveUser("Ivan", "Ivanov", (byte) 25);
+        userService.saveUser("Olya", "Orlova", (byte) 30);
+        userService.saveUser("Stepan", "Stepanov", (byte) 35);
+        userService.saveUser("Aleksey", "Petrov", (byte) 20);
+
+        List<User> userList = userService.getAllUsers();
+        userList.stream().forEach(e -> System.out.println(e.toString()));
+
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
+
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,33 @@
 package jm.task.core.jdbc.util;
 
+
+import jm.task.core.jdbc.MySQLConnector;
+
+import java.sql.Connection;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+
+    private static String url = "jdbc:mysql://127.0.0.1";
+    private static String port = "3306";
+    private static String user = "root";
+    private static String dbName = "mydb";
+    private static String password = "root";
+
+    public static Connection getConnection() {
+        try {
+            MySQLConnector mySqlConnector = new MySQLConnector(url, port, dbName, user, password);
+            return mySqlConnector.getConnection();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static void main(String args[]) {
+
+        System.out.println(getConnection());
+    }
+
+
 }
